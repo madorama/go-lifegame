48cc4ddd4a877a5394c0e256171cd2994262e056
madorama <mamunabo@hotmail.co.jp>
2021-07-09 23:07:01 +0900

    ✨ Create lifegame random initialize function

--- lifegame.go
+++ lifegame.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 type CellStatus uint
 
 const Dead CellStatus = 0
@@ -9,6 +14,22 @@ type Lifegame struct {
 	world [][]CellStatus
 }
 
-func main() {
+func randomInit(width uint, height uint) *Lifegame {
+	var game = new(Lifegame)
+	game.world = make([][]CellStatus, height)
+	for iy := 0; iy < int(height); iy++ {
+		for ix := 0; ix < int(width); ix++ {
+			var cell = Alive
+			if rand.Float32() >= 0.3 {
+				cell = Dead
+			}
+			game.world[iy] = append(game.world[iy], cell)
+		}
+	}
+	return game
+}
 
+func main() {
+	var game = *randomInit(50, 50)
+	fmt.Println(&game.world)
 }
